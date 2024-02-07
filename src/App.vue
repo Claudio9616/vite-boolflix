93fa9e15c6cfc0a4d1768f62bff03966df440db7
Claudio9616 <clares9611@gmail.com>
2024-02-07 17:16:18 +0100

    add store

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script>
+import AppMain from './components/AppMain.vue';
+import { store } from './store'
+const endpointMovie = 'https://api.themoviedb.org/3/search/movie?api_key=edcc8a8669f8819a0394b2c8f9a3038c&query=superman'
 import axios from 'axios';
 export default {
-  name: 'App Boolflix'
+  name: 'App Boolflix',
+  data: () => ({
+    store
+  }),
+  components: { AppMain },
+  methods: {
+    fetchMovieStore() {
+      (axios.get(endpointMovie).then(res => {
+        const movies = res.data.results.map(res => {
+          return {
+            id: res.id,
+            title: res.title,
+            originalTitle: res.original_title,
+            language: res.original_language,
+            vote: res.vote_average,
+            text: res.overview,
+            date: res.release_date,
+            img: res.poster_path
+          }
+        })
+        store.movies = movies
+      }))
+    },
+    fetchSeriesStore() {
+
+    }
+  }
+
+
 }
 </script>
 <template>
-  <div class="container"></div>
+  <AppMain @click-button="fetchMovieStore" />
+  <div>
+    <!-- fare registrazione sul sito dei film edcc8a8669f8819a0394b2c8f9a3038c
+      {{ ricorda che dopo il query= ci va il nome che l'utente sta cercando }}
+      {{ ricorda che per le img guarda l'indirizzo su milestone 3 poi gli metti w342 e poi gli monti di fianco ciò che arriva dall'api
+    ricordati che devi inserire tutto in una costante fino al w342 e poi gli monti api }}
+    guardare bene postman
+  fare uno store con 2 array uno per i film e uno per le serie tv -->
+  </div>
 </template>
 <style lang="scss">
 @use './assets/stylesass/style.scss';
---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+import { store } from '../store';
+export default {
+    data: () => ({
+        store
+    }),
+    emits: ['click-button']
+}
+</script>
+<template>
+    <button @click="$emit('click-button')"></button>
+</template>
+<style></style>
