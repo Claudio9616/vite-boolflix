d2c2432e7f321c4f916a88deafe379e78493864d
Claudio9616 <clares9611@gmail.com>
2024-02-07 17:48:33 +0100

    200 ok axios

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppMain from './components/AppMain.vue';
 import { store } from './store'
 const endpointMovie = 'https://api.themoviedb.org/3/search/movie?api_key=edcc8a8669f8819a0394b2c8f9a3038c&query=superman'
+const endpointSeries = 'https://api.themoviedb.org/3/search/tv?api_key=edcc8a8669f8819a0394b2c8f9a3038c&query=superman'
 import axios from 'axios';
 export default {
   name: 'App Boolflix',
@@ -25,10 +26,30 @@ export default {
           }
         })
         store.movies = movies
+        console.log(movies)
       }))
     },
     fetchSeriesStore() {
-
+      (axios.get(endpointSeries).then(res => {
+        const series = res.data.results.map(res => {
+          return {
+            id: res.id,
+            title: res.name,
+            originalTitle: res.original_name,
+            language: res.original_language,
+            vote: res.vote_average,
+            text: res.overview,
+            date: res.first_air_date,
+            img: res.poster_path
+          }
+        })
+        store.series = series
+        console.log(series)
+      }))
+    },
+    fetchAllStore() {
+      this.fetchMovieStore()
+      this.fetchSeriesStore()
     }
   }
 
@@ -36,15 +57,15 @@ export default {
 }
 </script>
 <template>
-  <AppMain @click-button="fetchMovieStore" />
-  <div>
-    <!-- fare registrazione sul sito dei film edcc8a8669f8819a0394b2c8f9a3038c
+  <AppMain @click-button="fetchAllStore" />
+  <!-- fare una funzione che richiama le due fetch axios al click del bottone -->
+
+  <!-- fare registrazione sul sito dei film edcc8a8669f8819a0394b2c8f9a3038c
       {{ ricorda che dopo il query= ci va il nome che l'utente sta cercando }}
       {{ ricorda che per le img guarda l'indirizzo su milestone 3 poi gli metti w342 e poi gli monti di fianco ciò che arriva dall'api
     ricordati che devi inserire tutto in una costante fino al w342 e poi gli monti api }}
     guardare bene postman
   fare uno store con 2 array uno per i film e uno per le serie tv -->
-  </div>
 </template>
 <style lang="scss">
 @use './assets/stylesass/style.scss';
