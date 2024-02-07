91cf4cf4b72a10285bce26e12af6e84be56ccf14
Claudio9616 <clares9611@gmail.com>
2024-02-07 15:18:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script>
+import axios from 'axios';
+export default {
+  name: 'App Boolflix'
+}
+</script>
+<template>
+  <div class="container"></div>
+</template>
+<style lang="scss">
+@use './assets/stylesass/style.scss';
+</style>
